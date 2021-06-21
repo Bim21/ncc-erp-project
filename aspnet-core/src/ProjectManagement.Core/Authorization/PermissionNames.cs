c3ab24e2218f493b1e6d750ac11ec13eff0b8acb
duong.trankhanh <duong.trankhanh.ncc@gmail.com>
2021-06-21 14:55:40 +0700

    #16952 Thêm api cho client và tree permission

--- aspnet-core/src/ProjectManagement.Core/Authorization/PermissionNames.cs
+++ aspnet-core/src/ProjectManagement.Core/Authorization/PermissionNames.cs
@@ -1,4 +1,8 @@
-﻿namespace ProjectManagement.Authorization
+﻿using Abp.MultiTenancy;
+using System.Collections.Generic;
+using static ProjectManagement.Authorization.Roles.StaticRoleNames;
+
+namespace ProjectManagement.Authorization
 {
     public static class PermissionNames
     {
@@ -7,5 +11,86 @@
         public const string Pages_Users = "Pages.Users";
 
         public const string Pages_Roles = "Pages.Roles";
+
+        public const string Admin = "Admin";
+
+        public const string Admin_Client = "Admin.Client";
+        public const string Admin_Client_ViewAll = "Admin.Client.ViewAll";
+        public const string Admin_Client_Create = "Admin.Client.Create";
+        public const string Admin_Client_Edit = "Admin.Client.Edit";
+        public const string Admin_Client_Delete = "Admin.Client.Delete";
+
+    }
+    public class GrantPermissionRoles
+    {
+        public static Dictionary<string, List<string>> PermissionRoles = new Dictionary<string, List<string>>()
+        {
+            {
+                Host.Admin,
+                new List<string>()
+                {
+                    PermissionNames.Pages_Users,
+                    PermissionNames.Pages_Tenants,
+                    PermissionNames.Pages_Roles,
+                    PermissionNames.Admin,
+
+                    //Client
+                    PermissionNames.Admin_Client,
+                    PermissionNames.Admin_Client_ViewAll,
+                    PermissionNames.Admin_Client_Create,
+                    PermissionNames.Admin_Client_Edit,
+                    PermissionNames.Admin_Client_Delete,
+
+                    
+                }
+            }
+        };
+        public class SystemPermission
+        {
+            public string Name { get; set; }
+            public MultiTenancySides MultiTenancySides { get; set; }
+            public string DisplayName { get; set; }
+            public bool IsConfiguration { get; set; }
+            public List<SystemPermission> Childrens { get; set; }
+            public static List<SystemPermission> ListPermissions = new List<SystemPermission>()
+            {
+                new SystemPermission{ Name =  PermissionNames.Pages_Users, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "Users" },
+                new SystemPermission{ Name =  PermissionNames.Pages_Roles, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "Roles" },
+                new SystemPermission{ Name =  PermissionNames.Pages_Tenants, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "Tenants" },
+                new SystemPermission{ Name =  PermissionNames.Admin, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "Admin" },
+
+                //Client
+                 new SystemPermission{ Name =  PermissionNames.Admin_Client, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "Client" },
+                 new SystemPermission{ Name =  PermissionNames.Admin_Client_ViewAll, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "View All Client" },
+                 new SystemPermission{ Name =  PermissionNames.Admin_Client_Create, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "Create New Client" },
+                 new SystemPermission{ Name =  PermissionNames.Admin_Client_Edit, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "Edit Client" },
+                 new SystemPermission{ Name =  PermissionNames.Admin_Client_Delete, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "Delete Client" },
+
+            };
+            public static List<SystemPermission> TreePermissions = new List<SystemPermission>()
+            {
+                //Admin
+                new SystemPermission { Name =  PermissionNames.Admin, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "Admin",
+                    Childrens = new List<SystemPermission>() {
+                        new SystemPermission { Name = PermissionNames.Pages_Tenants, MultiTenancySides = MultiTenancySides.Host, DisplayName = "Tenants" },
+                        new SystemPermission { Name = PermissionNames.Pages_Users, MultiTenancySides = MultiTenancySides.Host, DisplayName = "Users" },
+                        new SystemPermission { Name = PermissionNames.Pages_Roles, MultiTenancySides = MultiTenancySides.Host, DisplayName = "Roles" },
+
+                        //Client
+                        new SystemPermission { Name =  PermissionNames.Admin_Client, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "Client",
+                            Childrens = new List<SystemPermission>()
+                            {
+                                 new SystemPermission{ Name =  PermissionNames.Admin_Client_ViewAll, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "View All Client" },
+                                 new SystemPermission{ Name =  PermissionNames.Admin_Client_Create, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "Create Client" },
+                                 new SystemPermission{ Name =  PermissionNames.Admin_Client_Edit, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "Edit Client" },
+                                 new SystemPermission{ Name =  PermissionNames.Admin_Client_Delete, MultiTenancySides = MultiTenancySides.Host | MultiTenancySides.Tenant, DisplayName = "Delete Client" },
+                            }
+                        },
+                    }
+                },
+
+            };
+
+        }
     }
 }
