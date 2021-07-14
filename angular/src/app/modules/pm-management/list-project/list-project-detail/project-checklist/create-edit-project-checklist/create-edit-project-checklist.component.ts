d84850e6d2a5421a4549827a975f5f5f05e30aaa
huyentrang <gatay1408@gmail.com>
2021-07-14 13:37:54 +0700

    #16994 cai tien project checklist

--- angular/src/app/modules/pm-management/list-project/list-project-detail/project-checklist/create-edit-project-checklist/create-edit-project-checklist.component.ts
+++ angular/src/app/modules/pm-management/list-project/list-project-detail/project-checklist/create-edit-project-checklist/create-edit-project-checklist.component.ts
@@ -1,4 +1,4 @@
-import { AppComponentBase } from 'shared/app-component-base';
+import { AppComponentBase } from '@shared/app-component-base';
 import { ChecklistService } from './../../../../../../service/api/checklist.service';
 import { catchError, filter } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
@@ -7,6 +7,7 @@ import { ChecklistTitleDto, projectChecklistDto } from './../../../../../../serv
 import { ChecklistCategoryService } from './../../../../../../service/api/checklist-category.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Component, OnInit, Inject, Injector } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-create-edit-project-checklist',
@@ -16,11 +17,13 @@ import { Component, OnInit, Inject, Injector } from '@angular/core';
 export class CreateEditProjectChecklistComponent extends AppComponentBase implements OnInit {
 
   public listItems: ChecklistTitleDto[] = [];
+public listProjectType: string[] = Object.keys(this.APP_ENUM.ProjectType);
 
   public projectChecklist = {} as projectChecklistDto;
   public projectId: any;
   public checklistItemId: any;
   public listChecklistItemId: any;
+  public checkAll: boolean = false;
 
 
 
@@ -52,13 +55,53 @@ export class CreateEditProjectChecklistComponent extends AppComponentBase implem
       checkListItemId: this.checklistItemId,
       isActive: false,
     }
-    this.projectChecklistService.create(item).pipe(catchError(this.projectChecklistService.handleError)).subscribe((res) => {
+    this.arrCheckLists=this.arrCheckLists.map(element => {
+      return element.id
+    });
+    this.projectChecklistService.addCheckListItemByProject(this.projectId,this.arrCheckLists).pipe(catchError(this.projectChecklistService.handleError)).subscribe((res) => {
       abp.notify.success("created outcomeRequest ");
-      this.dialogRef.close(item);
+      this.dialogRef.close(this.arrCheckLists);
       this.isLoading = false;
     }, () => this.isLoading = false);
 
 
+
+  }
+  
+
+  public getByEnum(enumValue: number, enumObject: any) {
+    for (const key in enumObject) {
+      if (enumObject[key] == enumValue) {
+        return key;
+      }
+    }
+  }
+  public checkAllItems:number;
+  public arrCheckLists:any=[];
+ 
+
+  checkAllTask($e: MatCheckboxChange) {
+    this.listItems.forEach(el => {
+      el.createMode = $e.source.checked;
+      this.arrCheckLists.push(el);
+
+
+    })
+    this.checkAllItems = $e.source.checked ? 1 : 0;
+  }
+  updateCheckAllStatus(item) {
+    if (this.listItems.every(el =>{ el.createMode == false;})) {
+      this.checkAllItems = 0;
+      this.arrCheckLists.splice(item)
+    }
+    else if (this.listItems.every(el => {el.createMode == true;})) {
+      this.checkAllItems = 1;
+      this.arrCheckLists.push(item)
+    }
+    else {
+      this.checkAllItems = 2;
+      this.arrCheckLists.push(item)
+    }
   }
 
 }
