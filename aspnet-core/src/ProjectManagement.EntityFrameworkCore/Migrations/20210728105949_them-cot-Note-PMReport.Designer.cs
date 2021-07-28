dd4d7a3bd7d182c3046ef5f0187434f3ae0f5f2c
BuiHoangLam <lam.buihoang.ncc@gmail.com>
2021-07-28 18:56:24 +0700

    #18612 Cải tiến Project > Delivery Management > Weekly Report
    - Thêm cột note cho pmreport
    - Trả về số lượng project tương ứng trạng thái khi get all paging pmreport
    - Thêm get all pmreport
    - Thêm update note cho pmreport

--- aspnet-core/src/ProjectManagement.EntityFrameworkCore/Migrations/20210728105949_them-cot-Note-PMReport.Designer.cs
+++ aspnet-core/src/ProjectManagement.EntityFrameworkCore/Migrations/20210728105949_them-cot-Note-PMReport.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectManagement.EntityFrameworkCore;
 
 namespace ProjectManagement.Migrations
 {
     [DbContext(typeof(ProjectManagementDbContext))]
-    partial class ProjectManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210728105949_them-cot-Note-PMReport")]
+    partial class themcotNotePMReport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -2335,12 +2337,6 @@ namespace ProjectManagement.Migrations
                         .HasColumnType("tinyint");
 
                     b.Property<int?>("TenantId")
-                        .HasColumnType("int");
-
-                    b.Property<int>("TotalNormalWorkingTime")
-                        .HasColumnType("int");
-
-                    b.Property<int>("TotalOverTime")
                         .HasColumnType("int");
 
                     b.HasKey("Id");
