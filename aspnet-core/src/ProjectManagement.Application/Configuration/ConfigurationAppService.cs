dbb9edd80eae205ddca8ed0db24b983032a5b3a7
duong.trankhanh <duong.trankhanh.ncc@gmail.com>
2021-07-30 13:59:52 +0700

    #18088 thêm Configuration

--- aspnet-core/src/ProjectManagement.Application/Configuration/ConfigurationAppService.cs
+++ aspnet-core/src/ProjectManagement.Application/Configuration/ConfigurationAppService.cs
@@ -28,6 +28,14 @@ namespace ProjectManagement.Configuration
                 ClientAppId = await SettingManager.GetSettingValueForApplicationAsync(AppSettingNames.ClientAppId),
                 SecurityCode = await SettingManager.GetSettingValueForApplicationAsync(AppSettingNames.SecurityCode),
                 SecretCode = await SettingManager.GetSettingValueForApplicationAsync(AppSettingNames.SecretCode),
+                FinanceUri = await SettingManager.GetSettingValueForApplicationAsync(AppSettingNames.FinaceUri),
+                FinanceSecretKey = await SettingManager.GetSettingValueForApplicationAsync(AppSettingNames.FinanceSecretKey),
+                TimesheetUri = await SettingManager.GetSettingValueForApplicationAsync(AppSettingNames.TimesheetUri),
+                TimesheetSecretKey = await SettingManager.GetSettingValueForApplicationAsync(AppSettingNames.TimesheetSecretKey),
+                CanSendDay = await SettingManager.GetSettingValueForApplicationAsync(AppSettingNames.CanSendDay),
+                CanSendHour = await SettingManager.GetSettingValueForApplicationAsync(AppSettingNames.CanSendHour),
+                ExpiredDay = await SettingManager.GetSettingValueForApplicationAsync(AppSettingNames.ExpiredDay),
+                ExpiredHour = await SettingManager.GetSettingValueForApplicationAsync(AppSettingNames.ExpiredHour),
             };
         }
 
@@ -36,13 +44,31 @@ namespace ProjectManagement.Configuration
         {
             if (string.IsNullOrEmpty(input.ClientAppId) ||
                 string.IsNullOrEmpty(input.SecurityCode) ||
-                string.IsNullOrEmpty(input.SecretCode))
+                string.IsNullOrEmpty(input.SecretCode) ||
+                string.IsNullOrEmpty(input.FinanceUri) ||
+                string.IsNullOrEmpty(input.FinanceSecretKey) ||
+                string.IsNullOrEmpty(input.TimesheetUri) ||
+                string.IsNullOrEmpty(input.TimesheetSecretKey) ||
+                string.IsNullOrEmpty(input.CanSendDay) ||
+                string.IsNullOrEmpty(input.CanSendHour) ||
+                string.IsNullOrEmpty(input.ExpiredDay) ||
+                string.IsNullOrEmpty(input.ExpiredHour)
+                )
             {
                 throw new UserFriendlyException("All setting values need to be completed");
+
             }
             await SettingManager.ChangeSettingForApplicationAsync(AppSettingNames.ClientAppId, input.ClientAppId);
             await SettingManager.ChangeSettingForApplicationAsync(AppSettingNames.SecurityCode, input.SecurityCode);
             await SettingManager.ChangeSettingForApplicationAsync(AppSettingNames.SecretCode, input.SecretCode);
+            await SettingManager.ChangeSettingForApplicationAsync(AppSettingNames.FinaceUri, input.FinanceUri);
+            await SettingManager.ChangeSettingForApplicationAsync(AppSettingNames.FinanceSecretKey, input.FinanceSecretKey);
+            await SettingManager.ChangeSettingForApplicationAsync(AppSettingNames.TimesheetUri, input.TimesheetUri);
+            await SettingManager.ChangeSettingForApplicationAsync(AppSettingNames.TimesheetSecretKey, input.TimesheetSecretKey);
+            await SettingManager.ChangeSettingForApplicationAsync(AppSettingNames.CanSendDay, input.CanSendDay);
+            await SettingManager.ChangeSettingForApplicationAsync(AppSettingNames.CanSendHour, input.CanSendHour);
+            await SettingManager.ChangeSettingForApplicationAsync(AppSettingNames.ExpiredDay, input.ExpiredDay);
+            await SettingManager.ChangeSettingForApplicationAsync(AppSettingNames.ExpiredHour, input.ExpiredHour);
             return input;
         }
     }
