f01d9721fe0106af13b0b50bc41f6fbf28491245
xuan.vuduy <xuan.vuduy@ncc.asia>
2021-08-04 11:20:18 +0700

    #18610 fix bug trang PM, DM

--- angular/src/app/modules/pm-management/list-project/list-project-detail/weekly-report/weekly-report.component.ts
+++ angular/src/app/modules/pm-management/list-project/list-project-detail/weekly-report/weekly-report.component.ts
@@ -163,6 +163,7 @@ export class WeeklyReportComponent extends AppComponentBase implements OnInit {
   }
   public cancelWeekReport() {
     this.processWeekly = false;
+    this.isEditWeeklyReport =false;
     this.getWeeklyReport();
   }
   updateWeekReport(report) {
@@ -232,6 +233,7 @@ export class WeeklyReportComponent extends AppComponentBase implements OnInit {
     }
   }
   public cancelFutureReport() {
+    this.isEditFutureReport= false;
     this.processFuture = false;
     this.getFuturereport();
   }
@@ -328,6 +330,7 @@ export class WeeklyReportComponent extends AppComponentBase implements OnInit {
   public cancelProblemReport() {
     this.processProblem = false;
     this.getProjectProblem();
+    this.isEditProblem =false;
   }
   public editProblemReport(user: projectUserDto) {
     this.isEditProblem = true;
