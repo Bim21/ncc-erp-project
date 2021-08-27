ecd4f4d232bb14bac8991f67cbfe522f7429920a
thiendoduc <thien.doduc.ncc@gmail.com>
2021-08-27 16:12:26 +0700

    #18202 thêm phaseId vào param của getAllPhaseSub và getAllPhaseMain

--- aspnet-core/src/ProjectManagement.Application/APIs/CheckPointUserResults/CheckPointUserResultAppService.cs
+++ aspnet-core/src/ProjectManagement.Application/APIs/CheckPointUserResults/CheckPointUserResultAppService.cs
@@ -17,10 +17,9 @@ namespace ProjectManagement.APIs.RequestLevel
     public class CheckPointUserResultAppService : ProjectManagementAppServiceBase
     {
         [HttpPost]
-        public async Task<GridResult<CheckPointUserResultDto>> GetAllPagingMain(GridParam input)
+        public async Task<GridResult<CheckPointUserResultDto>> GetAllPagingMain(GridParam input,long phaseId)
         {
-            var phaseId = (long)input.FilterItems.FirstOrDefault(x => x.PropertyName == "phaseId").Value;
-            var isPhaseMain = await WorkScope.GetAll<Phase>().AnyAsync(x => x.Id == Convert.ToInt64(phaseId) && x.Type == PhaseType.Main);
+            var isPhaseMain = await WorkScope.GetAll<Phase>().AnyAsync(x => x.Id == phaseId && x.Type == PhaseType.Main);
             var cput = WorkScope.GetAll<CheckPointUserResultTag>()
                 .Select(x => new ResultTagDto
                 {
