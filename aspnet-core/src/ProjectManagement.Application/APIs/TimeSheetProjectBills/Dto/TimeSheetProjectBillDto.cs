7a23ceead5a82d6ac6cf759fb310474da921a203
TranLinhcbx9 <60964536+TranLinhcbx9@users.noreply.github.com>
2021-10-22 16:08:14 +0700

    #20739 [PROJECT > Timesheets] [FE+BE] Cải tiến trang Timesheets > Timesheet detail

---
+++ aspnet-core/src/ProjectManagement.Application/APIs/TimeSheetProjectBills/Dto/TimeSheetProjectBillDto.cs
@@ -0,0 +1,26 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using NccCore.Anotations;
+using ProjectManagement.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static ProjectManagement.Constants.Enum.ProjectEnum;
+
+namespace ProjectManagement.APIs.TimeSheetProjectBills.Dto
+{
+    [AutoMapTo(typeof(TimesheetProjectBill))]
+    public class TimeSheetProjectBillDto : EntityDto<long>
+    {
+        public long ProjectId { get; set; }
+        public long UserId { get; set; }
+        public string BillRole { get; set; }
+        public float BillRate { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public CurrencyCode Currency { get; set; }
+        public string Note { get; set; }
+        public string ShadowNote { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
