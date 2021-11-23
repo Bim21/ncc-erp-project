3a3d5aed280676be85acfeb19db74de5aad00d0b
trang.danghuyen.ncc <trang.danghuyen.ncc@gmail.com>
2021-11-23 13:37:40 +0700

    #22902 fix bug filter checklist item

--- angular/src/app/modules/checklist-management/checklist/checklist.component.ts
+++ angular/src/app/modules/checklist-management/checklist/checklist.component.ts
@@ -60,11 +60,13 @@ export class ChecklistComponent extends PagedListingComponentBase<any> implement
   public readonly FILTER_CONFIG: InputFilterDto[] = [
     { propertyName: 'name', comparisions: [0, 6, 7, 8], displayName: "Name" },
     { propertyName: 'code', comparisions: [0, 6, 7, 8], displayName: "Code" },
-    { propertyName: 'title', comparisions: [0, 6, 7, 8], displayName: "Title" },
+    { propertyName: 'categoryName', comparisions: [0, 6, 7, 8], displayName: "Category Name" },
     { propertyName: 'mandatory', comparisions: [3], displayName: "Mandatory" , filterType : 3 , dropdownData: this.projectTypeForFilter },
     { propertyName: 'personInCharge', comparisions: [0, 6, 7, 8], displayName: "Person in Charge" },
     { propertyName: 'auditTarget', comparisions: [0, 6, 7, 8], displayName: "auditTarget" },
     { propertyName: 'note', comparisions: [0, 6, 7, 8], displayName: "note" },
+    { propertyName: 'description', comparisions: [0, 6, 7, 8], displayName: "description" },
+
   ];
   
 
