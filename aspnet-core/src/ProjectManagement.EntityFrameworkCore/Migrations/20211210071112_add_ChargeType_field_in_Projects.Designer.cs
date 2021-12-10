0d2556c4c0b06b0dd00426a36a1ee1afa6ddff6b
Lê Hiếu <59021855+hieuldev@users.noreply.github.com>
2021-12-10 17:17:03 +0700

    #23672 Project > PM Management > Thêm mới/sửa project > Thêm mới trường Charge Type

--- aspnet-core/src/ProjectManagement.EntityFrameworkCore/Migrations/20211210071112_add_ChargeType_field_in_Projects.Designer.cs
+++ aspnet-core/src/ProjectManagement.EntityFrameworkCore/Migrations/20211210071112_add_ChargeType_field_in_Projects.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectManagement.EntityFrameworkCore;
 
 namespace ProjectManagement.Migrations
 {
     [DbContext(typeof(ProjectManagementDbContext))]
-    partial class ProjectManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211210071112_add_ChargeType_field_in_Projects")]
+    partial class add_ChargeType_field_in_Projects
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
