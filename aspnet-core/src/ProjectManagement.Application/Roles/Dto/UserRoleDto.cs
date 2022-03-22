3dcfac31679d502fdc17277101330200d55ab780
thunv <thu.nguyenvan@ncc.asia>
2022-03-22 16:25:49 +0700

    Merge branch 'dev' of https://ops.nccsoft.vn/DefaultCollection/NCC-ERP/_git/ncc-erp-project into Task-27586

---
+++ aspnet-core/src/ProjectManagement.Application/Roles/Dto/UserRoleDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static ProjectManagement.Constants.Enum.ProjectEnum;
+
+namespace ProjectManagement.Roles.Dto
+{
+    public class UserRoleDto
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string AvatarPath { get; set; }
+        public UserType UserType { get; set; }
+        public UserLevel UserLevel { get; set; }
+        public Branch Branch { get; set; }
+    }
+}
