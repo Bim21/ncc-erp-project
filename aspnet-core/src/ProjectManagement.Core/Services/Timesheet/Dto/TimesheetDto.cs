2586c33ed7b94be665b185eadc249431da7bb87f
Manh Vuong <manh.vuongtan@ncc.asia>
2022-04-12 07:41:19 +0700

    29441 Project > Timesheet > Detail > Export Invoice For Tax

--- aspnet-core/src/ProjectManagement.Core/Services/Timesheet/Dto/TimesheetDto.cs
+++ aspnet-core/src/ProjectManagement.Core/Services/Timesheet/Dto/TimesheetDto.cs
@@ -1,64 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static ProjectManagement.Constants.Enum.ProjectEnum;
 
 namespace ProjectManagement.Services.Timesheet.Dto
 {
-    public class TimesheetDto
+    public class TimesheetDetailDto
     {
-        public string EmailAddress { get; set; }
-        public string TaskName { get; set; }
-        public string ProjectName { get; set; }
-        public string ProjectCode { get; set; }
-        public int WorkingMinute { get; set; }
-        public string Note { get; set; }
         public DateTime DateAt { get; set; }
+        public string DateAtView { get; set; }
+        public float WorkingTime { get; set; }
+        public string TaskName { get; set; }
+        public string Note { get; set; }
+        public string EmailAddress { get; set; }
+        public string ProjectCode { get; set; }
     }
 
     public class TimesheetTaxDto
     {
         public List<DateTime> ListWorkingDay { get; set; }
-        public List<TimesheetDto> ListTimesheet { get; set; }
+        public List<TimesheetDetailDto> ListTimesheet { get; set; }
     }
 
     public class ProjectInvoice
     {
         public string ProjectCode { get; set; }
         public string ProjectName { get; set; }
-        public List<MemberOfProject> ListMemberOfProject { get; set; }
     }
-    public class MemberOfProject
-    {
-        public string EmailAddress { get; set; }
-        public string FullName { get; set; }
-        public float WorkingTimeDay { get; set; }
-        public double BillRate { get; set; }
-        public double LineTotal { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime? EndTime { get; set; }
-        public List<TimesheetDto> ListTimesheetOfUserInProject { get; set; }
-    }
-
-    public class ResultProjectInvoice
+   
+   
+    public class ResultInvoice
     {
         public List<string> ListProjectCode { get; set; }
-        public List<ProjectInvoice> ListProjectInvoice { get; set; }
+        public List<InvoiceDto> ListInvoice { get; set; }
     }
-    public class TimesheetDetailOfUser
+    public class InvoiceDto
     {
+        public string ProjectCode { get; set; }
+        public string EmailAddress { get; set; }
         public string FullName { get; set; }
         public string ProjectName { get; set; }
         public float WorkingTimeDay { get; set; }
-        public double BillRate { get; set; }
-        public double LineTotal { get; set; }
-        public List<TimesheetDetail> ListTimesheetDetail { get; set; }
-    }
-
-    public class TimesheetDetail
-    {
-        public string DateAt { get; set; }
-        public float WorkingTime { get; set; }
-        public string TaskName { get; set; }
-        public string Note { get; set; }
+        public float? TotalWorkingDay { get; set; }
+        public int DefaultWorkingHours { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public ChargeType ChargeType { get; set; }
+        public float BillRate { get; set; }
+        public double LineTotal
+        {
+            get
+            {
+                return WorkingTimeDay * BillRate;
+            }
+        }
+        public List<TimesheetDetailDto> ListTimesheetDetail { get; set; }
     }
 }
