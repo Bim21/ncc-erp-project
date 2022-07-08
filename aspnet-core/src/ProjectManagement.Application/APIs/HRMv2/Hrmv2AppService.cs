089d979b78b55a44d9d04474912b489d754cdba9
trang.dangthihuyen <trang.dangthihuyen@ncc.asia>
2022-07-08 13:54:20 +0700

    #33678 upload avatar

---
+++ aspnet-core/src/ProjectManagement.Application/APIs/HRMv2/Hrmv2AppService.cs
@@ -0,0 +1,36 @@
+﻿using Abp.Authorization;
+using Amazon.Runtime.Internal.Util;
+using Microsoft.AspNetCore.Mvc;
+using NccCore.IoC;
+using ProjectManagement.APIs.HRMv2.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.APIs.HRMv2
+{
+    public class Hrmv2AppService : ProjectManagementAppServiceBase
+    {
+        [AbpAllowAnonymous]
+        [HttpPost]
+        public async Task UpdateAvatarFromHrm(UpdateAvatarDto input)
+        {
+            if (string.IsNullOrEmpty(input.AvatarPath))
+            {
+                Logger.Error($"user with {input.AvatarPath} is null or empty");
+                return;
+            }
+            var user = await GetUserByEmailAsync(input.EmailAddress);
+
+            if (user == null)
+            {
+                Logger.Error($"not found user with email {input.EmailAddress}");
+                return;
+            }
+
+            user.AvatarPath = input.AvatarPath;
+            await WorkScope.UpdateAsync(user);
+        }
+    }
+}
