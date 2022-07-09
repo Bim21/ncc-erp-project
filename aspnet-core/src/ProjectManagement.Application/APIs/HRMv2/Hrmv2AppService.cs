c26c2b4b63e8e24e46d8dc4d9d41cd1bba828d31
trang.dangthihuyen <trang.dangthihuyen@ncc.asia>
2022-07-09 10:10:47 +0700

    #33678 upload avatar

--- aspnet-core/src/ProjectManagement.Application/APIs/HRMv2/Hrmv2AppService.cs
+++ aspnet-core/src/ProjectManagement.Application/APIs/HRMv2/Hrmv2AppService.cs
@@ -1,8 +1,11 @@
 ﻿using Abp.Authorization;
+using Abp.UI;
 using Amazon.Runtime.Internal.Util;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NccCore.IoC;
 using ProjectManagement.APIs.HRMv2.Dto;
+using ProjectManagement.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,10 +15,17 @@ namespace ProjectManagement.APIs.HRMv2
 {
     public class Hrmv2AppService : ProjectManagementAppServiceBase
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public Hrmv2AppService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         [AbpAllowAnonymous]
         [HttpPost]
         public async Task UpdateAvatarFromHrm(UpdateAvatarDto input)
         {
+            CheckSecurityCode();
             if (string.IsNullOrEmpty(input.AvatarPath))
             {
                 Logger.Error($"user with {input.AvatarPath} is null or empty");
@@ -31,6 +41,16 @@ namespace ProjectManagement.APIs.HRMv2
 
             user.AvatarPath = input.AvatarPath;
             await WorkScope.UpdateAsync(user);
+        }
+        private void CheckSecurityCode()
+        {
+            var secretCode = SettingManager.GetSettingValue(AppSettingNames.SecurityCode);
+            var header = _httpContextAccessor.HttpContext.Request.Headers;
+            var securityCodeHeader = header["X-Secret-Key"].ToString();
+            if (secretCode == securityCodeHeader)
+                return;
+
+            throw new UserFriendlyException($"SecretCode does not match! {secretCode.Substring(0, secretCode.Length / 2)} != {securityCodeHeader.Substring(0, securityCodeHeader.Length / 2)}");
         }
     }
 }
