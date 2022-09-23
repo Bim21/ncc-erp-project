9c7893f53a44b2233e3dfac68db63b2e1d90d2d7
thunv <thu.nguyenvan@ncc.asia>
2022-09-23 14:42:42 +0700

    update get date deadline

--- aspnet-core/src/ProjectManagement.Application/APIs/TimesheetProjects/TimesheetProjectAppService.cs
+++ aspnet-core/src/ProjectManagement.Application/APIs/TimesheetProjects/TimesheetProjectAppService.cs
@@ -996,7 +996,7 @@ namespace ProjectManagement.APIs.TimesheetProjects
                     Year = invoice.Info.Year,
                     NameInvoice = $"Invoice number {invoice.Info.InvoiceNumber} tháng {invoice.Info.Month}/{invoice.Info.Year}",
                     TransferFee = invoice.Info.TransferFee,
-                    Deadline = invoice.Info.PaymentDueByStr()
+                    Deadline = invoice.Info.PaymentDueByDate()
                 });
             }
             var rs = await _financeService.CreateAllInvoices(listInvoices);
