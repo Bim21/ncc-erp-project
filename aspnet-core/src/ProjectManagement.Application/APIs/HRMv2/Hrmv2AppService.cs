320b5b510a5ddca4e02bd12fc09a3ff49910b08b
trang.dangthihuyen <trang.dangthihuyen@ncc.asia>
2022-11-24 17:42:46 +0700

    #38090 check connect to other tools

--- aspnet-core/src/ProjectManagement.Application/APIs/HRMv2/Hrmv2AppService.cs
+++ aspnet-core/src/ProjectManagement.Application/APIs/HRMv2/Hrmv2AppService.cs
@@ -69,7 +69,7 @@ namespace ProjectManagement.APIs.HRMv2
             if (secretCode == securityCodeHeader)
                 return;
 
-            throw new UserFriendlyException($"SecretCode does not match! {secretCode.Substring(0, secretCode.Length / 2)} != {securityCodeHeader.Substring(0, securityCodeHeader.Length / 2)}");
+            throw new UserFriendlyException($"SecretCode does not match: ProjectCode: {secretCode.Substring(secretCode.Length - 3)} != {securityCodeHeader}");
         }
         private async Task<ProjectManagement.Entities.Branch> GetBranchByCode(string code)
         {
@@ -598,6 +598,20 @@ namespace ProjectManagement.APIs.HRMv2
         private async Task SendKomu(StringBuilder sb)
         {
             await SendKomu(sb.ToString());
+        }
+
+        [HttpGet]
+        [AbpAllowAnonymous]
+        public GetResultConnectDto CheckConnectToHRM()
+        {
+            CheckSecurityCode();
+            return new GetResultConnectDto()
+            {
+                IsConnected = true,
+                Message = "Connected"
+            };
+
+
         }
     }
 
