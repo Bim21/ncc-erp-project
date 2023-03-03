9dedd7128212c01280eb6b5357ab5fdb6bd465e2
Trần Tiến Thành <Trn Tin Thnh>
2023-03-03 14:33:38 +0700

    Merged PR 23022: update va fix bug data
    Bỏ require PM note để submit weekly report,
    bỏ automation note
    bỏ Rate phải khác 0 mới save đc theo req của chị Mai
    Migrate lại theo Enum mới cho vào seed data chạy 1 lần duy nhất PR sau sẽ bỏ seed này .
    
    Related work items: #42029

---
+++ aspnet-core/src/ProjectManagement.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/MigrateProjectHealth.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Abp.Timing;
+
+namespace ProjectManagement.EntityFrameworkCore.Seed.Host
+{
+    public class MigrateProjectHealth
+    {
+        /// <summary>
+        /// Update lại Project Health khi sửa lại value Enum 0,1,2 -> 1,2,3
+        /// </summary>
+        private readonly ProjectManagementDbContext _context;
+
+        public MigrateProjectHealth(ProjectManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create()
+        {
+            MigrateProjectHealthNewEnum();
+        }
+
+        private void MigrateProjectHealthNewEnum()
+        {
+            var oldPMReportProject = _context.PMReportProjects
+                .Where(x => x.CreationTime.Date < new DateTime(2023, 02, 22, 0, 0, 0, 0))
+                .ToList();
+            oldPMReportProject.ForEach(x =>
+            {
+                x.ProjectHealth += 1;
+            });
+            _context.SaveChanges();
+        }
+    }
+}
